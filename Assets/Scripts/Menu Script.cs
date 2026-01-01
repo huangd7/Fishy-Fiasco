01544178f31b04200fe3c111c19ad7a894412285
huangd7 <huangd7@bxscience.edu>
2026-01-01 15:24:18 -0500

    Added menu, instructions, game scene
    menu fully functional, quit button works, instructions done.

---
+++ Assets/Scripts/Menu Script.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void InstructionButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnBackButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
