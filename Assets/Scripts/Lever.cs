f6f2b33204433ae8592a7ba3f44b8655e449676f
chene21-max <chene21@bxscience.edu>
2026-01-04 21:29:05 -0500

    Spawning in of the Levers

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private static System.Random rand = new System.Random();
+
+    public GameObject Goal; 
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        SpawnLevers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnLevers()
+    {
+
+        int[] Levers = new int [3];
+        for (int x = 0; x < 3; x++)
+        {
+            Levers[x] = rand.Next(0, 8);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if(Levers[i] == 0)
+            {
+                Vector3 position = new Vector3(152, -7, 2);
+                Instantiate(Goal, position, Quaternion.identity);
+            }
+            if (Levers[i] == 1)
+            {
+                Instantiate(Goal, new Vector3(90, -7, 0), Quaternion.identity);
+            }
+            if (Levers[i] == 2)
+            {
+                Instantiate(Goal, new Vector3(270, -7, 43), Quaternion.identity);
+            }
+            if (Levers[i] == 3)
+            {
+                Instantiate(Goal, new Vector3(285, -7, 120), Quaternion.identity);
+            }
+            if (Levers[i] == 4)
+            {
+                Instantiate(Goal, new Vector3(42, -7, 90), Quaternion.identity);
+            }
+            if (Levers[i] == 5)
+            {
+                Instantiate(Goal, new Vector3(0, -7, 224), Quaternion.identity);
+            }
+            if (Levers[i] == 6)
+            {
+                Instantiate(Goal, new Vector3(75, -7, 255), Quaternion.identity);
+            }
+            if (Levers[i] == 7)
+            {
+                Instantiate(Goal, new Vector3(195, -7, 271), Quaternion.identity);
+            }
+
+        }
+
+        
+    }
+}
