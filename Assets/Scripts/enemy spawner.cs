e63da7cc591a80480bfbfe99218581109b6fd7c8
krishball <krish.bxsci@gmail.com>
2026-01-11 21:00:31 -0500

    more fishies
    goldfish + angler2

--- Assets/Scripts/enemy spawner.cs
+++ Assets/Scripts/enemy spawner.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
+
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject anglerfishPrefab;
     public GameObject sharkPrefab;
 
+    public GameObject fishType3Prefab;
+    public GameObject fishType4Prefab;
+
     public int anglerfishCount = 2;
     public int sharkCount = 2;
+    public int fishType3Count = 2;
+    public int fishType4Count = 2;
 
     public Vector3 center = Vector3.zero;
     public float spawnRadius = 30f;
+
+    public float spawnDelay = 1.5f;
 
     void Start()
     {
@@ -19,9 +27,12 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnAfterDelay()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(spawnDelay);
+
         SpawnMany(anglerfishPrefab, anglerfishCount);
         SpawnMany(sharkPrefab, sharkCount);
+        SpawnMany(fishType3Prefab, fishType3Count);
+        SpawnMany(fishType4Prefab, fishType4Count);
     }
 
     void SpawnMany(GameObject prefab, int count)
@@ -31,7 +42,7 @@ public class EnemySpawner : MonoBehaviour
         for (int i = 0; i < count; i++)
         {
             Vector3 pos = RandomPointOnNavMesh();
-            GameObject e = Instantiate(prefab, pos + Vector3.up * 0.2f, Quaternion.identity);
+            Instantiate(prefab, pos + Vector3.up * 0.2f, Quaternion.identity);
         }
     }
 
